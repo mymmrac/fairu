b7a8bdbfca53382a90b14167ef9668febc81accd
Artem Yadelskyi <artem.yadelskyi@connectwise.com>
2021-11-27 10:43:54 +0200

    Reading current path

--- main.go
+++ main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{}
+type model struct {
+	path string
+	err  error
+}
 
 func newModel() *model {
-	return &model{}
+	path := "."
+	if len(os.Args) == 2 {
+		path = os.Args[1]
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return &model{err: err}
+	}
+
+	exist, err := exists(absPath)
+	if err != nil {
+		return &model{err: err}
+	}
+
+	if !exist {
+		return &model{err: fmt.Errorf("file %s doesn't exist", absPath)}
+	}
+
+	return &model{
+		path: absPath,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -19,6 +44,14 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.err != nil {
+		if _, ok := msg.(tea.KeyMsg); ok {
+			return m, tea.Quit
+		}
+
+		return m, tea.ExitAltScreen
+	}
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
@@ -31,7 +64,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return "Hello World!\n"
+	if m.err != nil {
+		return fmt.Sprintf("Error occurred: %v\n\nPress any key to exit.\n", m.err)
+	}
+
+	return fmt.Sprintf("Current path: %s\n", m.path)
 }
 
 func main() {
